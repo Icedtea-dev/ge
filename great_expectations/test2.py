7c2fad9403fe3e7b0679558e70d667e450f0d504
Olusegun F <felix_olusegun@yahoo.com>
2023-05-07 14:54:19 +0100

    Added New Expectations and Checkpoint

--- great_expectations/test2.py
+++ great_expectations/test2.py
@@ -1,10 +1,16 @@
-from eprocurement_tables import replace_list
-from build_expectations import build_expectations_for_data_assets
-import yaml
 
-temp_schema = {"id":"INTEGER","tag":"STRING","create":"STRING"}
+import great_expectations as gx
+from great_expectations.core.batch import BatchRequest, RuntimeBatchRequest
+from build_expectations import build_expectations_for_data_assets,get_src_dtypes_from_postgres,load_json,dump_json
+from eprocurement_tables import eproc_tables,process
 
-# build_expectations_for_data_assets("eproc.tbl_purchase_order","xx",temp_schema)
+context = gx.get_context()
 
-print(list(replace_list.keys()))
+dataset_id = 'deep-contact-361614'
 
+datasource_name = "BigQuery_datasource"
+
+available_data_asset_names = context.datasources[datasource_name].get_available_data_asset_names(
+    data_connector_names="default_inferred_data_connector_name")["default_inferred_data_connector_name"]
+
+print(available_data_asset_names)
